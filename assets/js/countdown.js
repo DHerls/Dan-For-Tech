8e8a2914a6452e7726e361100e5406aed87fcd58
Dan Herlihy <djherls@gmail.com>
2017-03-06 13:38:38 -0500

    Add GoodSearch and Countdown sections

---
+++ assets/js/countdown.js
@@ -0,0 +1,70 @@
+	function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function updateTimer(){
+	if (beforeTHON){
+		t = getTimeRemaining(start2018);
+		if (t.total <= 0){
+			beforeTHON = false;
+			clockSetup()
+		}
+	} else if (!afterTHON){
+		t = getTimeRemaining(end2018);
+		if (t.total <= 0){
+			afterTHON = true;
+			clockSetup()
+		}
+	} else {
+		t = getTimeRemaining(start2019);
+	}
+	daysSpan.innerHTML = t.days
+	hoursSpan.innerHTML = t.hours
+	minutesSpan.innerHTML = t.minutes
+	secondsSpan.innerHTML = t.seconds
+}
+
+function clockSetup(firstTime = false){
+	var html =  "<div id=\"clockdiv\"><h1>"
+	html += (beforeTHON || afterTHON ? "COUNTDOWN TO THON" : "HOURS LEFT IN THON");
+	html +="</h1><div><span class=\"days\"></span><div class=\"smalltext\">Days</div></div><div><span class=\"hours\"></span><div class=\"smalltext\">Hours</div></div><div><span class=\"mins\"></span><div class=\"smalltext\">Minutes</div></div><div><span class=\"secs\"></span><div class=\"smalltext\">Seconds</div>  </div></div>"
+	if (firstTime){
+		document.getElementById('countdown').innerHTML = html;
+	} else {
+		clock.innerHTML = html;
+	}
+	clock = document.getElementById("clockdiv");
+	daysSpan = clock.querySelector('.days');
+	hoursSpan = clock.querySelector('.hours');
+	minutesSpan = clock.querySelector('.mins');
+	secondsSpan = clock.querySelector('.secs');
+}
+		
+var start2018 = "February 16, 2018 6:00 PM GMT-0500";
+var end2018 = "February 18, 2017 4:00 PM GMT-0500"
+var start2019 = "February 15, 2018 6:00 PM GMT-0500";
+
+var beforeTHON = getTimeRemaining(start2018).total >= 0;
+var afterTHON = !beforeTHON && getTimeRemaining(end2018).total <= 0;
+
+var clock;
+var daysSpan;
+var hoursSpan;
+var minutesSpan;
+var secondsSpan;
+
+clockSetup(true);
+
+updateTimer()
+setInterval(updateTimer, 1000);
--- assets/js/danfortech.js
+++ assets/js/danfortech.js
@@ -3,10 +3,10 @@ $('.carousel').carousel({
 });
 
 var numTechChartAppeared = false;
+var goodsearchAppeared = false;
 
 $('#numTechChart').appear();
 $('#numTechChart').on('disappear', function(event, $all_appeared_elements) {
-	console.log("Disappeared");
 	numTechChartAppeared = false;
 });
 $('#numTechChart').on('appear', function(event, $all_appeared_elements) {
@@ -48,6 +48,7 @@ $('#numTechChart').on('appear', function(event, $all_appeared_elements) {
 		},
 		data: [
 		{
+			xValueFormatString: "####",
 			type: "column",
 			dataPoints: [
 				  { x: 2005, y: 0 },
@@ -66,6 +67,90 @@ $('#numTechChart').on('appear', function(event, $all_appeared_elements) {
 				  { x: 2018, y: 1 }
 			]
 		}
+		]
+	});
+});
+
+$('#goodsearchChart').appear();
+$('#goodsearchChart').on('disappear', function(event, $all_appeared_elements) {
+	goodsearchAppeared = false;
+});
+$('#goodsearchChart').on('appear', function(event, $all_appeared_elements) {
+	if (goodsearchAppeared){
+		return;
+	} else {
+		goodsearchAppeared = true;
+	}
+
+	$("#goodsearchChart").CanvasJSChart({
+		title: {
+			text: "Goodsearch Revenue by THON Year"
+		},
+		animationEnabled: true,
+		animationDuration: 2000,
+		axisY:{
+            valueFormatString:"$###.00",
+        },
+		 toolTip:{
+			content:"{label} {name}: {y}" ,
+		},
+		data: [
+		{
+			type: "line",
+			yValueFormatString:"$###.00",
+			name: "2014",
+			showInLegend: true,
+			dataPoints: [
+				{ y: 81.63, label: "September"},
+				{ y: 135.14, label: "October"},
+				{ y: 80.17, label: "November"},
+				{ y: 62.50, label: "December"},
+				{ y: 44.65, label: "January"},
+				{ y: 28.49, label: "February"},
+			]
+		},
+		{
+			type: "line",
+			yValueFormatString:"$###.00",
+			name: "2015",
+			showInLegend: true,
+			dataPoints: [
+				{ y: 53.81, label: "September"},
+				{ y: 74.90, label: "October"},
+				{ y: 35.47, label: "November"},
+				{ y: 57.27, label: "December"},
+				{ y: 87.51, label: "January"},
+				{ y: 65.51, label: "February"},
+			]
+		},
+		{
+			type: "line",
+			name: "2016",
+			yValueFormatString:"$###.00",
+			showInLegend: true,
+			dataPoints: [
+				{ y: 16.81, label: "September"},
+				{ y: 11.79, label: "October"},
+				{ y: 8.55, label: "November"},
+				{ y: 9.10, label: "December"},
+				{ y: 7.76, label: "January"},
+				{ y: 5.41, label: "February"},
+			]
+		},
+				{
+			type: "line",
+			name: "2017",
+			yValueFormatString:"$###.00",
+			showInLegend: true,
+			dataPoints: [
+				{ y: 16.88, label: "September"},
+				{ y: 162.37, label: "October"},
+				{ y: 184.26, label: "November"},
+				{ y: 119.93, label: "December"},
+				{ y: 104.51, label: "January"},
+				{ y: 109.55, label: "February"},
+			]
+		},
 		]
 	});
 });
